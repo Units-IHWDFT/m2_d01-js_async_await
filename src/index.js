f5b3264df8e5526abe47d4452ba18cd0bc2dd099
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 16:32:59 +0200

    Example - Fetch data using async await

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-// Using fetch with Promise syntax - then() and catch()
-fetch('https://api.spacexdata.com/v4/launches')
-  .then((response) => { 
-    return response.json();
-  })
-  .then((jsonResponse) => {
+// Using fetch with async/await 
+async function displayMissionPatches() {
+  try {
+    const response = await fetch('https://api.spacexdata.com/v4/launches');
+    const jsonResponse = await response.json();
+
     console.log("Parsed response", jsonResponse);
-  })
-  .catch((err) => console.log("Something went wrong!", error));
+  } catch (err) {
+    // Handle error or a rejected Promise
+    console.log("Something went wrong!", error);
+  }
+}
+
+displayMissionPatches();
