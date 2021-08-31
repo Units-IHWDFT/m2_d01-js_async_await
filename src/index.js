d316a5192db267d64659a69d93cef5030804660e
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 16:24:26 +0200

    Example - Previous lesson example with fetch

--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
-const directions = [
-  'Starting point: Ironhack Paris',
-  '← Head northwest on Bd Voltaire toward Rue Léon Frot',
-  '← Turn left onto Rue Chanzy',
-  '* Café Titon, 34 Rue Titon, 75011 Paris, France'
-];
-
-
-function obtainDirections(step) {
-  return new Promise (function (resolve, reject) {
-    setTimeout(() => {
-      console.log( directions[step] );
-
-      if (!directions[step]) reject('Instructions not found.')
-      else resolve();
-    }, 2000); 
-    
+// Using fetch with Promise syntax - then() and catch()
+fetch('https://api.spacexdata.com/v4/launches')
+  .then((response) => { 
+    return response.json();
   })
-}
-
-async function getCoffee() {
-  try {
-    await obtainDirections(0);
-    await obtainDirections(1);
-    await obtainDirections(2);
-    await obtainDirections(3);
-    
-    console.log('You arrived at your destination!');
-  } catch(err) {
-    console.log(err)
-  } 
-}
-
-
-getCoffee();
+  .then((jsonResponse) => {
+    console.log("Parsed response", jsonResponse);
+  })
+  .catch((err) => console.log("Something went wrong!", error));
