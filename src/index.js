6ee883b2ce0a87d6750ee82d7a94d3f45a751314
ross-u <cirkovic.uros.srb@gmail.com>
2021-08-31 16:36:18 +0200

    Example - Fetch and display data using async await

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 // Using fetch with async/await 
-async function displayMissionPatches() {
+async function displayMissionPatches(limit = 0) {
+
   try {
     const response = await fetch('https://api.spacexdata.com/v4/launches');
     const jsonResponse = await response.json();
 
-    console.log("Parsed response", jsonResponse);
-  } catch (err) {
+    console.log(jsonResponse);
+    const launchesToDisplay = jsonResponse.slice(0, limit);
+    
+    launchesToDisplay.forEach((launchObj) => {
+      const patchImage = launchObj.links.patch.small;
+      const imgElement = document.createElement("img");
+
+      imgElement.setAttribute("src", patchImage);
+      imgElement.setAttribute("width", 200);
+      document.body.appendChild(imgElement);
+    });
+    
+  } catch (error) {
     // Handle error or a rejected Promise
     console.log("Something went wrong!", error);
   }
 }
 
-displayMissionPatches();
+displayMissionPatches(10);
